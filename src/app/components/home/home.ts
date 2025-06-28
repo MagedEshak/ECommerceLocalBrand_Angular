081108732e8f9ef29795ae27e1d3925a0db897cd
Maged Eshak <magedeshak36@gmail.com>
2025-06-28 05:07:55 +0300

    home , header, footer, splash

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { from } from 'rxjs';
 import { Header } from '../header/header';
 import { Footer } from '../footer/footer';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Footer],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
