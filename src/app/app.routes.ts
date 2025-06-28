23384ee40493ae81f811a377deafe7e1389c9e84
Maged Eshak <magedeshak36@gmail.com>
2025-06-28 18:48:26 +0300

    Product details design page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Splash } from './components/splash/splash';
 import { Home } from './components/home/home';
 import { NotFound } from './components/not-found/not-found';
+import { ProductDetails } from './components/product-details/product-details';
 
 export const routes: Routes = [
   { path: '', component: Splash },
   { path: 'home', component: Home },
+  { path: 'product-details', component: ProductDetails },
 
 
   //Notfound
--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrls: ['./header.css']
 })
 export class Header {
+  constructor(public router: Router) {
 
+  }
+
+  get isHome(): boolean {
+    return this.router.url === '/' || this.router.url === '/home' ;
+  }
 }
